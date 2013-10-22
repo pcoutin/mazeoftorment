acf115bbdfa91fd2d358410345efb09c5bb2c075
Pedro L Coutin <pcoutin@ucsd.edu>
2013-10-22 00:21:31 -0700

    Header file

---
+++ server/mot_maze.h
@@ -0,0 +1,34 @@
+#ifndef _MT_MAZE_H
+#define _MT_MAZE_H
+
+typedef unsigned char      MCELL;
+
+struct
+{
+   MCELL *data;
+
+   /*
+    * w and h are the "data w": not the number of cells in a row or
+    * column, but the number of cells AND walls in a row or column.
+    */
+   size_t w, h;
+
+   /*
+    * Cell width and height of the maze, not counting walls.
+    */
+   unsigned int width, height;
+
+   /*
+    * The killer's/drunkard's/maze carver's current X and Y position.
+    * (Used for generating)
+    */
+   unsigned int X, Y;
+
+   /*
+    * Data size of the maze representation, in bytes.
+    */
+   size_t size;
+} MAZE;
+
+
+#endif
