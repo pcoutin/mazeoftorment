35c3c8fff56c646197234b840f8d8e87a056d57b
Pedro L Coutin <pcoutin@ucsd.edu>
2013-10-23 01:16:15 -0700

    Player file, kind of. TODO.

---
+++ client/TODO.md
@@ -0,0 +1,7 @@
+* Game menus, connecting to a server. Server discovery with a broadcast
+packet. Server list menu?
+NETWORKING.
+
+* Game mechanics. Implement the predator, prey in lua. Make a "Player"
+class compatible with lua in C somehow, but predator and prey
+kinds/"implements". Set image in lua.
---
+++ client/entities/player.c
@@ -0,0 +1,42 @@
+/*
+ * Make some kind of struct thing compatible with lua for the 2 player
+ * types? (prey and hunter)
+ * Implement prey and hunter more in lua? Movement is the same for both so
+ * just implement that in C. Game mechanics, winning, etc in lua.
+ *
+ * HAVE error messages for not being able to open an image instead of oops
+ * segfault who cares if couldn't open
+ */
+
+typedef struct Player
+{
+   PICTURE pic;
+   unsigned int x, y;
+   void (*update) (Player *self);
+};
+
+/*
+ * draws and updates player.
+ */
+static void
+update(Player *self)
+{
+   Uint8 *state = SDL_GetKeyboardFromState(int *numkeys);
+
+   /*
+    * Move if a key was pressed.
+    */
+   if (state[SDL_SCANCODE_UP])
+   {
+      self->y--; // not really
+   }
+   if (state[SDL_SCANCODE_DOWN])
+   {
+   }
+   if (state[SDL_SCANCODE_LEFT])
+   {
+   }
+   if (state[SDL_SCANCODE_RIGHT])
+   {
+   }
+}
